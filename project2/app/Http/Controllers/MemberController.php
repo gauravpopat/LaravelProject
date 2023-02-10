9344f6ed24a809e40645fdf7629695d5ec639f1f
gaurav <gauravp@zignuts.com>
2023-02-10 16:00:16 +0530

    Performed Getter Accessor Casting

--- project2/app/Http/Controllers/MemberController.php
+++ project2/app/Http/Controllers/MemberController.php
@@ -1,13 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Member;
 use Illuminate\Http\Request;
 
 class MemberController extends Controller
 {
     public function index()
     {
-        return "Hello";
+        return Member::all();
+    }
+
+    public function accessor()
+    {
+        //return Member::find(1)->name;
+        return Member::find(1)->city_name;
+    }
+
+    public function casting()
+    {
+        return Member::all();
     }
 }
---
+++ project2/database/seeders/MemberSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Database\Factories\MemberFactory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Member;
+
+class MemberSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Member::factory(5)->create();
+    }
+}
